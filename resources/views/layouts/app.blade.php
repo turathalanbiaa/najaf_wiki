1831f8c0e87c684db4db99830be46f324f23c26d
turathalanbiaa <turath.alanbiaa@gmail.com>
2019-07-15 11:06:46 +0300

    Edit app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>موسوعة النجف</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/jquery-3.4.1.min.js') }}" ></script>
