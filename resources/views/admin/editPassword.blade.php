3996cc509fa918eee91caf26f24d01fc3e52339c
turathalanbiaa <turath.alanbiaa@gmail.com>
2019-07-21 10:38:07 +0300

    Correct update password route

--- resources/views/admin/editPassword.blade.php
+++ resources/views/admin/editPassword.blade.php
@@ -7,7 +7,7 @@
                     <div class="card-header">تغير الباسورد</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('update-password',$user->id) }}">
+                        <form method="POST" action="{{ route('updatePassword',$user->id) }}">
                             @csrf
                             <div class="form-group row">
                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('الاسم') }}</label>
